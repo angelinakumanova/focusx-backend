cede344505fe84f904027c55a3c05018c5acbc62
Andjelina Kumanova <angelinakumanova@gmail.com>
2025-08-21 20:58:33 +0300

    Update CorsConfig.java

--- user-service-focusx/src/main/java/app/focusx/config/CorsConfig.java
+++ user-service-focusx/src/main/java/app/focusx/config/CorsConfig.java
@@ -5,12 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
