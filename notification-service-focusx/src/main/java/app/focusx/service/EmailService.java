b7aad6b50cbda6b36646c8b5470c0dc37a7f19b1
Andjelina Kumanova <angelinakumanova@gmail.com>
2025-09-26 01:10:33 +0300

    Update mail sending api

--- notification-service-focusx/src/main/java/app/focusx/service/EmailService.java
+++ notification-service-focusx/src/main/java/app/focusx/service/EmailService.java
@@ -7,27 +7,34 @@ import app.focusx.model.Notification;
 import app.focusx.model.NotificationStatus;
 import app.focusx.model.NotificationType;
 import app.focusx.repository.NotificationRepository;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Service
 @Slf4j
 public class EmailService {
+    @Value("${SENDGRID_API_KEY}")
+    private String SENDGRID_API_KEY;
 
     private final NotificationRepository notificationRepository;
-    private final JavaMailSender sender;
     private final SpringTemplateEngine templateEngine;
 
-    public EmailService(NotificationRepository notificationRepository, JavaMailSender sender, SpringTemplateEngine templateEngine) {
+    public EmailService(NotificationRepository notificationRepository, SpringTemplateEngine templateEngine) {
         this.notificationRepository = notificationRepository;
-        this.sender = sender;
         this.templateEngine = templateEngine;
     }
 
@@ -53,29 +60,34 @@ public class EmailService {
         sendMail(event.getContact(), subject, body);
     }
 
-    private void sendMail(String email, String subject, String body) {
+    private void sendMail(String toEmail, String subject, String body) {
 
         Notification notification = Notification.builder()
                 .id(UUID.randomUUID().toString())
                 .type(NotificationType.EMAIL)
-                .contact(email)
+                .contact(toEmail)
                 .status(NotificationStatus.PENDING)
                 .createdOn(LocalDateTime.now())
                 .build();
 
+        Email from = new Email("foocusx@outlook.com");
+        Email to = new Email(toEmail);
+        Content content = new Content("text/html", body);
+        Mail mail = new Mail(from, subject, to, content);
+
+        SendGrid sg = new SendGrid(SENDGRID_API_KEY);
+
+        Request request = new Request();
         try {
-            sender.send(mimeMessage -> {
-                MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, true);
-                messageHelper.setFrom("foocusx@outlook.com");
-                messageHelper.setTo(email);
-                messageHelper.setSubject(subject);
-                messageHelper.setText(body, true);
-            });
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            sg.api(request);
 
             notification.setStatus(NotificationStatus.DELIVERED);
-        } catch (Exception e) {
+        } catch (IOException ex) {
             notification.setStatus(NotificationStatus.FAILED);
-            log.warn("Failed to send mail to user with email: %s due to %s".formatted(email, e.getMessage()));
+            log.warn("Failed to send mail to user with email: %s due to %s".formatted(toEmail, ex.getMessage()));
         }
 
         notificationRepository.save(notification);
